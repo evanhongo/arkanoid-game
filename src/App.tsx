48d5846a81b08aa19f6c3f50d34ad2b8e39e5a11
evanhongo <evanhongo@gmail.com>
2021-08-29 22:37:03 +0800

    refactor: remove useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react'
+import React, { useRef, useEffect } from 'react'
 import useInterval from '@/util/useInterval';
 import { useImmer } from "use-immer";
 
